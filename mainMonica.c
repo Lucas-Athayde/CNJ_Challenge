f46be97c368b7115610912d5d5c306efcaee6d9d
Lucas-Athayde <lucas.master97@hotmail.com>
2021-01-25 16:57:50 -0300

    Update nos comentarios e exercicio

--- mainMonica.c
+++ mainMonica.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+	A idade de Dona Mônica
+	Dona Mônica é mãe de três filhos que têm idades diferentes. 
+	Ela notou que, neste ano, a soma das idades dos seus três filhos é igual à idade dela. 
+	Neste problema, dada a idade de dona Mônica e as idades de dois dos filhos, seu programa deve computar e imprimir a idade do filho mais velho.
+	https://olimpiada.ic.unicamp.br/pratique/pj/2019/f1/idade/
+*/
+
+
 /* Nesta função iremos calcular a idade do 3 filho.
 Nela, temos como parametro 3 valores, que serão 'monica', 'idade1' e 'idade2'
 Sabemos que a soma dos 3 filhos é a idade de Dona Monica
@@ -66,11 +75,11 @@ int main()
         
     } while(M < 40 || M > 110);
     
-    /*
-	Neste laço de repetição entramos no mesmo conceito, contudo agora devemos ter a idade de um dos filhos
-    não podendo ser MAIOR ou IGUAL a idade de Dona Monica e NEM MENOR que 1 ano de idade.
-    Caso sejam cumpridas as condições, ele sai do loop e pula para o proximo passo.
-	*/
+/*
+Neste laço de repetição entramos no mesmo conceito, contudo agora devemos ter a idade de um dos filhos
+não podendo ser MAIOR ou IGUAL a idade de Dona Monica e NEM MENOR que 1 ano de idade.
+Caso sejam cumpridas as condições, ele sai do loop e pula para o proximo passo.
+*/
     do
     {
         printf("Digite a idade do primeiro filho: ");
@@ -91,11 +100,11 @@ int main()
     } while(filho1 < 1 || filho1 >= M );
     
     
-    /*
-	Enfim temos nosso terceiro laço de repetição Do While
-    Faremos o mesmo processo, contudo agora sua idade não pode ser igual a do filho anterior(filho1)
-    Então não pode ser MAIOR, IGUAL do que de Dona Monica, MENOR do que 1 e nem IGUAL a do outro filho.
-	*/
+/*
+Enfim temos nosso terceiro laço de repetição Do While
+Faremos o mesmo processo, contudo agora sua idade não pode ser igual a do filho anterior(filho1)
+Então não pode ser MAIOR, IGUAL do que de Dona Monica, MENOR do que 1 e nem IGUAL a do outro filho.
+*/
     do
 	{
 		printf("Digite a idade do segundo filho: ");
@@ -119,26 +128,28 @@ int main()
         
     } while(filho2 < 1 || filho2 >= M || filho2 == filho1);
     
-    /*
-	Printamos os valores que inserimos no começo das operações
-    Sendo o primeiro o valor da idade de Dona Monica
-    E o segundo as idades de 2 dos filhos
-	*/
+/*
+Printamos os valores que inserimos no começo das operações
+Sendo o primeiro o valor da idade de Dona Monica
+E o segundo as idades de 2 dos filhos
+*/
     printf("A idade de Dona Monica e: %d\n", M);
     printf("e a idade de 2 dos filhos são: %d e %d",filho1,filho2);
     
-    /*Aqui chamamos a função 'idadeFilho3' que esta lá em cima e armazenamos na variavel 'filho3'
-    Passamos como parametro os valores adquiridos e informados anteriormente, 'M', 'filho1' e 'filho2'
-	Com isto, retornamos o valor 'idade3' que foi calculado dentro da função e printado*/
+/*
+Aqui chamamos a função 'idadeFilho3' que esta lá em cima e armazenamos na variavel 'filho3'
+Passamos como parametro os valores adquiridos e informados anteriormente, 'M', 'filho1' e 'filho2'
+Com isto, retornamos o valor 'idade3' que foi calculado dentro da função e printado
+*/
     filho3 = idadeFilho3(M, filho1, filho2);
     printf("\n\nA idade do 3 filho e: %d anos\n\n ", filho3);
     
-    /*
-	Seguimos para o passo final que é retornar qual o filho mais velho dos 3.
-    Para isto, temos a função 'maiorIdade', que recebe 3 parametros, sendo eles a idade dos 3 filhos
-    'filho1', 'filho2' e 'filho3'.
-    A explicação da função se encontra no inicio do codigo, onde a mesma foi criada	
-	*/
+/*
+Seguimos para o passo final que é retornar qual o filho mais velho dos 3.
+Para isto, temos a função 'maiorIdade', que recebe 3 parametros, sendo eles a idade dos 3 filhos
+'filho1', 'filho2' e 'filho3'.
+A explicação da função se encontra no inicio do codigo, onde a mesma foi criada	
+*/
     idadeCalc = maiorIdade(filho1, filho2, filho3);
     printf("O filho mais velho tem %d anos", idadeCalc);	//Printamos a maior idade dentre os 3 filhos de Dona Monica.
 }
